7e7db3f3e239cc56b06f4364eb6789f163c48f02
palanurag002 <palanurag002@gmail.com>
2026-08-01 18:01:42 +0530

    updated

---
+++ Module-2/Session-2/hello.c
@@ -0,0 +1,11 @@
+//HEADER: Includes the standard input-output library for the printf function.
+#include<stdio.h>
+//MAIN FUNCTION: The primary entery point where program execution begins.
+main()
+{
+	//Prints the message to the console screen 
+	printf("Hello, Instagram World!\n");
+	
+	//RETURN STATEMENT: Terminates the main function and signals success (0) to
+	return 0;
+}
